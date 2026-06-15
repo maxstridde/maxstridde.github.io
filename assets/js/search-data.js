acbac8bba509a75545d12180fb323940491d78dd
maxstridde <maxstr@posteo.de>
2026-06-15 20:44:42 +0000

    Deploying to gh-pages from @ maxstridde/maxstridde.github.io@a8f020e7e2c33b99dcc7e1cae50917172f1e092b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/books/";
               },
-            },{id: "dropdown-news",
-              title: "News",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/news/";
-              },
             },{id: "dropdown-wie-funktionierts",
               title: "Wie funktionierts?",
               description: "",
@@ -51,7 +44,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "https://www.linkedin.com/in/max-stridde/";
               },
-            },{id: "post-wie-optimiert-man-seine-waschkosten-eine-mathematische-analyse",
+            },{id: "post-mobilität-in-mailand",
+        
+          title: "Mobilität in Mailand",
+        
+        description: "Ein Überblick über die verschiedenen Verkehrsmittel und Fortbewegungsmöglichkeiten in Mailand.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/Mobilit%C3%A4t-in-Mailand/";
+          
+        },
+      },{id: "post-wie-optimiert-man-seine-waschkosten-eine-mathematische-analyse",
         
           title: "Wie optimiert man seine Waschkosten? Eine mathematische Analyse",
         
@@ -133,22 +137,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/small_things_like_these/";
-            },},{id: "news-milano-ich-komme-die-olympischen-winterspiele-sind-gerade-vorrüber-der-frühlling-kann-kommen-ein-spannendes-semester-liegt-vor-mir-um-6-uhr-ging-es-heute-morgen-los-von-köln-um-16-00-hat-mich-mailand-willkommen-geheißen-ich-freue-mich-auf-die-zeit-die-vor-mir-liegt",
-          title: 'Milano, ich komme. Die Olympischen Winterspiele sind gerade vorrüber. Der Frühlling kann kommen....',
-          description: "",
-          section: "News",},{id: "news-heute-ist-ein-großer-tag-denn-ich-ziehe-in-mein-studentenwohnheim-vom-gemütlichen-familienleben-bei-carlo-gehts-jetzt-näher-an-meine-uni-dafür-werde-ich-ab-heute-mein-zimmer-teilen-müssen-ich-bin-gespannt-mit-wem-ich-bis-zum-sommer-in-einem-zimmer-schlafen-werde-und-freue-mich-darauf-mein-regal-und-meine-ecke-einzuräumen",
-          title: 'Heute ist ein großer Tag, denn ich ziehe in mein Studentenwohnheim. Vom gemütlichen...',
-          description: "",
-          section: "News",},{id: "news-ich-wünsche-allen-frauen-auf-der-welt-einen-schönen-tag-und-besonders-viele-blumen-sowie-kreative-ich-übernehme-heute-mal-das-putzen-oder-ich-bring-dir-einmal-frühstück-ans-bett-geschenkgutscheine-heute-morgen-habe-ich-mich-zu-männer-sind-schweine-rasiert-gleich-gehts-mit-freunden-in-die-biblioteca-ambrosiana-wo-frauen-heute-gratis-eintritt-bekommen-heut-abend-gehe-ich-mit-meinem-vater-der-in-mailand-zu-besuch-ist-lecker-essen-was-für-ein-schöner-tag",
-          title: 'Ich wünsche allen Frauen auf der Welt einen schönen Tag und besonders viele...',
-          description: "",
-          section: "News",},{id: "news-ich-halts-nicht-mehr-aus-in-italien-ich-brauche-eine-pause-in-deutschland-endlich-wieder-schwarzbrot-essen-endlich-wieder-regen-nein-spaß-für-ein-wissenschaftliches-kolleg-zum-thema-mathematische-optimierung-mache-ich-mich-auf-den-weg-in-die-schöne-stadt-weimar-die-uni-muss-eine-woche-lang-auf-mich-warten-ändern-lässt-sich-das-leider-nicht-ich-freue-mich-auf-die-arbeits-zeit-vor-ort-aber-auch-auf-die-rückkehr-nach-mailand-die-bahnreise-durch-die-diesmal-verschneite-schweiz-ist-für-mich-mal-wieder-mit-genüsslichen-blicken-aus-dem-fenster-verbunden",
-          title: 'Ich halts nicht mehr aus in Italien, ich brauche eine Pause in Deutschland....',
-          description: "",
-          section: "News",},{id: "news-freut-euch-und-wartet-noch-ein-wenig-länger-auf-den-nächsten-post-das-heißt-natürlich-nicht-dass-es-aktuell-nichts-zu-berichten-gäbe-ich-würde-gerne-sagen-dass-mir-bei-all-den-vielen-erlebnissen-gar-keine-zeit-bleibt-um-am-blog-zu-schreiben-während-dies-an-manchen-tagen-stimmt-kommt-mir-auch-die-erste-klausurphase-dazwischen-in-italien-gibt-es-bereits-zur-hälfte-des-semesters-prüfungen-die-auch-zur-hälfte-in-die-endnote-einzählen-obendrein-schreibe-ich-gerade-an-mehreren-texten-gleichzeitig-bald-geht-es-also-weiter",
-          title: 'Freut euch (und wartet) noch ein wenig länger auf den nächsten Post :)....',
-          description: "",
-          section: "News",},{id: "projects-kämpfen-für-die-liebe",
+            },},{id: "projects-kämpfen-für-die-liebe",
           title: 'Kämpfen für die Liebe',
           description: "Über die Verflechtungenen zwischen Kampfsport und Beziehungen.",
           section: "Projects",handler: () => {
